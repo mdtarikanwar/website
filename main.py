9980936fff4e2de9f3cb29d5813b925edafce170
mdtarikanwar <mdtarikanwar811gmail.com>
2021-12-26 13:20:29 +0530

    some comments added.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# importing required modules.
 import json
 import math
 import os
@@ -8,21 +9,29 @@ from flask import Flask, render_template, request, session, redirect
 from werkzeug.utils import secure_filename
 
 
+# stabilised connection to the Mysql
 con = connector.connect(user="root",
                         password="Belsarnifu30@",
                         host="localhost",
                         database="my_tables")
-
+# making a cursor for the execution of sql command.
 cr = con.cursor()
 
+# opening config.json file, this json file contains many attributes for the web pages.
 with open("config.json", "r") as f:
     params = json.load(f)["params"]
 
+# creating an object of Flask class (Flask App).
 app = Flask(__name__)
+
+# setting the secret_key attribute of app.
 app.secret_key = 'super-secrer-key'
+
+# setting the folder where all image file will be uploaded, which will come from json file.
 app.config['UPLOAD_FOLDER'] = params['upload_location']
 
 
+# making a route for the Hone page.
 @app.route("/")
 def index():
     cr.execute("select * from posts;")
@@ -51,6 +60,7 @@ def index():
     return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
 
 
+# making a route for contact page.
 @app.route("/contact", methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
@@ -71,11 +81,13 @@ def contact():
     return render_template('contact.html', params=params)
 
 
+# making a route for about page.
 @app.route("/about")
 def about():
     return render_template('about.html', params=params)
 
 
+# making a route for posts page.
 @app.route("/post/<string:post_slug>", methods=['GET'])
 def post_route(post_slug):
     cr.execute("select * from posts where slug='"+post_slug+"'")
@@ -83,6 +95,7 @@ def post_route(post_slug):
     return render_template('post.html', params=params, post=post)
 
 
+# making a route for admin page.
 @app.route("/admin", methods=['GET', 'POST'])
 def login():
     cr.execute("select * from posts;")
@@ -103,12 +116,14 @@ def login():
         return render_template('login.html', params=params)
 
 
+# making a route for logout page, which will redirect to the home page.
 @app.route('/logout')
 def logout():
     session.pop('user')
     return redirect('/')
 
 
+# making a route for edit page.
 @app.route('/edit/<string:S_No>', methods=['GET', 'POST'])
 def edit(S_No):
     if 'user' in session and session['user'] == params['admin_user_id']:
@@ -144,6 +159,7 @@ def edit(S_No):
         return render_template("edit.html", params=params, post=post, S_No=S_No, mystr=string)
 
 
+# making a route for delete page, which will redirect to the admin page.
 @app.route("/delete/<string:S_No>", methods=['GET', 'POST'])
 def delete(S_No):
     if 'user' in session and session['user'] == params['admin_user_id']:
@@ -153,6 +169,7 @@ def delete(S_No):
         return redirect('/admin')
 
 
+# making a route for file upload page, which will redirect to the admin page after file uploaded.
 @app.route("/uploader", methods=['POST', 'GET'])
 def uploader():
     if 'user' in session and session['user'] == params['admin_user_id']:
